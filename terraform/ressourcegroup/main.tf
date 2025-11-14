958b9dc83c2e2aac60840685a9dff7bfd9d773ac
frederic LEROY <74693233+freddblry@users.noreply.github.com>
2025-11-14 14:28:54 +0100

    Add main.tf: terraform/ressourcegroup/main.tf

---
+++ terraform/ressourcegroup/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = merge(
+    var.tags,
+    {
+      environment = var.environment
+      managed_by  = "terraform"
+    }
+  )
+}
